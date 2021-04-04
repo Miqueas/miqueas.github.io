f5025cffdf2bb8f230ad897d7ec460df09438e69
Miqueas Martinez <miqueas2020@yahoo.com>
2021-04-04 11:51:21 -0500

    Coloreada la página principal, cambios menores en el README, package.json y el layout y visibilidad de algunos elementos en la página principal

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 [![License][LicenseBadge]][licenseURL]
 
-### Build
+# My github pages 🤔
+
+## Build
 
 This website uses Pug for generate all the static content:
 
 ```
 npm install
-npm run-script build
+npm run build
 ```
 
 And that's it! `pug-cli` is used to compile the `.pug` files.
--- Static/Main.js
+++ Static/Main.js
@@ -1,3 +1,34 @@
+function query(name, url = window.location.href) {
+  name        = name.replace(/[\[\]]/g, '\\$&');
+  let regex   = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  let results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+let urlParams = {};
+(window.onpopstate = function () {
+    let match,
+        pl = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) {
+            return decodeURIComponent(s.replace(pl, " "));
+        },
+        query = window.location.search.substring(1);
+
+    while (match = search.exec(query)) {
+        if (decode(match[1]) in urlParams) {
+            if (!Array.isArray(urlParams[decode(match[1])])) {
+                urlParams[decode(match[1])] = [urlParams[decode(match[1])]];
+            }
+            urlParams[decode(match[1])].push(decode(match[2]));
+        } else {
+            urlParams[decode(match[1])] = decode(match[2]);
+        }
+    }
+})();
+
 let app = document.getElementById("app");
 const Raw = "https://raw.githubusercontent.com/Miqueas/Miqueas.github.io/master/";
 
@@ -21,3 +52,6 @@ en_btn.addEventListener("click", () => home("en"));
 es_btn.addEventListener("click", () => home("es"));
 en_btn_mobile.addEventListener("click", () => home("en"));
 es_btn_mobile.addEventListener("click", () => home("es"));
+
+console.log(query("foo"));
+console.log(urlParams);
