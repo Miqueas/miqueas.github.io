1b4a3607eca774cded85017bc9a1e1735131b135
Miqueas Martinez <miqueas2020@yahoo.com>
2021-04-09 22:57:26 -0500

    Rehabilitadas traducciones

--- Static/Main.js
+++ Static/Main.js
@@ -33,3 +33,18 @@ async function Update() {
 window.onload = Update;
 // Trigger on window.history.pushState function call
 window.onpopstate = Update;
+
+const en_btn        = document.getElementById("en-btn");
+const es_btn        = document.getElementById("es-btn");
+const en_btn_mobile = document.getElementById("en-btn-mobile");
+const es_btn_mobile = document.getElementById("es-btn-mobile");
+en_btn.addEventListener("click", () => Say.load("en"));
+es_btn.addEventListener("click", () => Say.load("es"));
+en_btn_mobile.addEventListener("click", () => Say.load("en"));
+es_btn_mobile.addEventListener("click", () => Say.load("es"));
+
+let mobile_burger = document.getElementById("mobile-burger");
+let mobile_menu = document.getElementById("mobile-menu");
+let mobile_menu_close_btn = document.getElementById("mobile-menu-close-btn");
+mobile_burger.addEventListener("click", () => mobile_menu.classList.toggle("is-active"));
+mobile_menu_close_btn.addEventListener("click", () => mobile_menu.classList.toggle("is-active"));
