f5760fc9208d0410278bfa497b8f7231d95ccf00
Miqueas <miqueas2020@yahoo.com>
2022-03-23 03:01:59 +0000

    Added VuePress config

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: "Miqueas Martinez",
+  dest: "www",
+  //public: "pub",
+
+  themeConfig: {
+    navbar: [
+      { text: "Docs", link: "/docs" },
+      { text: "Blog", link: "/blog" }
+    ]
+  }
+})
