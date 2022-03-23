356883175ff40e40a55f0a75322fb6766354cad2
Miqueas <>
2022-03-23 04:06:05 +0000

    Deploying to gh-pages from @ Miqueas/Miqueas.github.io@ad3f9c850f5d8f54e0e8ad5300905c90f82169d6 🚀

--- assets/index.html.09305bb2.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-8daa1a0e",path:"/",title:"",lang:"en-US",frontmatter:{home:!0,sidebar:!1,heroText:"Hi, I'm Miqueas!",tagline:"Lua, Linux and GTK enthusiast",footer:"Made with \u2764\uFE0F in Colombia"},excerpt:"",headers:[],git:{updatedTime:1648004545e3,contributors:[{name:"Miqueas",email:"miqueas2020@yahoo.com",commits:1}]},filePathRelative:"README.md"};export{e as data};
