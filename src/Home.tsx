5f717f5c70da808ece67c23281baf7c6c5f0111c
Miqueas <miqueas2020@yahoo.com>
2024-05-04 16:30:49 -0500

    Uh...

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+export default function Home() {
+  return (
+    <>
+      <img src="profile.png" alt="Profile picture" className="profile"/>
+      <h1>JOSUÉ MARTÍNEZ</h1>
+      <h3>DEVELOPER</h3>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = document.getElementById('root')
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+} else {
+  console.error('Root element not found')
+}
