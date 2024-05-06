93fdce11fdcfdc681b4324bcd7c0c7de2cf013a3
Miqueas <miqueas2020@yahoo.com>
2024-05-05 19:06:03 -0500

    H̷̙͍̯̥̾̒́ͮ͋͒E̼͇͈̙ͣͣ́͛̑̓̕L̹̜̽̈ͨ̽̅͆P̫̔͂ͫ̽

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,9 @@
-import './Home.css'
+import Styles from './Home.module.css'
 
 export default function Home() {
-  return (
-    <>
-      <img src="profile.png" alt="Profile picture" className="profile"/>
-      <h1>JOSUÉ MARTÍNEZ</h1>
-      <h3>DEVELOPER</h3>
-    </>
-  )
+  return (<section className={Styles['home']}>
+    <img src="profile.jpg" alt="Profile picture" className={Styles['profile']}/>
+    <h1>JOSUÉ MARTÍNEZ</h1>
+    <h3>DEVELOPER</h3>
+  </section>)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
 
 const root = document.getElementById('root')
 
@@ -10,10 +9,14 @@ if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/docs"/>
+          <Route path="/blog"/>
+        </Routes>
       </BrowserRouter>
     </React.StrictMode>
   )
 } else {
-  console.error('Root element not found')
+  throw new Error('No root element found')
 }
